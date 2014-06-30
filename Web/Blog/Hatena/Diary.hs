1e76c9b2de22f633f60bc625e05bffaaef67f5f0
Shinichiro Suzuki <shinichiro.su@gmail.com>
2014-06-30 19:47:44 +0900

    fixed type in blogtools.cabal

--- Web/Blog/Hatena/Diary.hs
+++ Web/Blog/Hatena/Diary.hs
@@ -6,11 +6,8 @@ import Network.URI
 import Data.Maybe (fromJust)
 
 -- | entryURIsOf
--- >>> entryURIsOf (HatenaDiary, fromJust $ parseURI "http://d.hatena.ne.jp/suzuki-shin/")
+-- >>> entryURIsOf $ fromJust $ parseURI "http://d.hatena.ne.jp/suzuki-shin/"
 -- [http://d.hatena.ne.jp/suzuki-shin/2014/06/02/hogehoge,http://d.hatena.ne.jp/suzuki-shin/2014/06/01/fugafuga]
 entryURIsOf :: URI -> IO [URI]
-entryURIsOf baseUri = return [(fromJust $ parseURI "http://d.hatena.ne.jp/suzuki-shin/2014/06/02/hogehoge"),
-                               (fromJust $ parseURI "http://d.hatena.ne.jp/suzuki-shin/2014/06/01/fugafuga")]
-
--- archivePageOf :: UserName -> Int -> IO String
--- archivePageOf user fromNum = openURL $ baseUrl ++ user ++ "/archive?of=" ++ show fromNum
+entryURIsOf baseUri = return [fromJust $ parseURI "http://d.hatena.ne.jp/suzuki-shin/2014/06/02/hogehoge",
+                               fromJust $ parseURI "http://d.hatena.ne.jp/suzuki-shin/2014/06/01/fugafuga"]
