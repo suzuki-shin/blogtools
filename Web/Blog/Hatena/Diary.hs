fea962230841949bd3772d233b52814a177c8ffb
Shinichiro Suzuki <shinichiro.su@gmail.com>
2014-06-30 21:08:22 +0900

    implement archiveURLs to Web.Blog.Hatena.Diary

--- Web/Blog/Hatena/Diary.hs
+++ Web/Blog/Hatena/Diary.hs
@@ -4,10 +4,26 @@ module Web.Blog.Hatena.Diary (
 
 import Network.URI
 import Data.Maybe (fromJust)
+import Text.XML.HXT.Core
+import Text.HandsomeSoup
+import Network.HTTP.Conduit (simpleHttp)
+import Data.ByteString.Lazy.Char8 as BL8 (unpack)
+import System.FilePath.Posix ((</>))
+import Control.Applicative ((<$>))
+import Data.List (nub)
 
 -- | entryURIsOf
 -- >>> entryURIsOf $ fromJust $ parseURI "http://d.hatena.ne.jp/suzuki-shin/"
 -- [http://d.hatena.ne.jp/suzuki-shin/2014/06/02/hogehoge,http://d.hatena.ne.jp/suzuki-shin/2014/06/01/fugafuga]
 entryURIsOf :: URI -> IO [URI]
-entryURIsOf baseUri = return [fromJust $ parseURI "http://d.hatena.ne.jp/suzuki-shin/2014/06/02/hogehoge",
-                               fromJust $ parseURI "http://d.hatena.ne.jp/suzuki-shin/2014/06/01/fugafuga"]
+entryURIsOf baseUri = return [ fromJust $ parseURI "http://d.hatena.ne.jp/suzuki-shin/2014/06/02/hogehoge", fromJust $ parseURI "http://d.hatena.ne.jp/suzuki-shin/2014/06/01/fugafuga" ]
+
+-- | archiveURLs
+-- >>> archiveURLs "http://d.hatena.ne.jp/hogehoge"
+-- ["http://d.hatena.ne.jp/hogehoge/archive?word=&of=50","http://d.hatena.ne.jp/hogehoge/archive?word=&of=100","http://d.hatena.ne.jp/hogehoge/archive?word=&of=150","http://d.hatena.ne.jp/hogehoge/archive?word=&of=200","http://d.hatena.ne.jp/hogehoge/archive?word=&of=250","http://d.hatena.ne.jp/hogehoge/archive?word=&of=300"]
+archiveURLs :: String -> IO [String]
+archiveURLs baseUrl = do
+  c <- simpleHttp $ baseUrl </> "archive"
+  let doc = readString [withParseHTML yes, withWarnings no] $ BL8.unpack c
+  links <- runX $ doc //> css "div" >>> hasAttrValue "id" (== "pager-top") >>> css "a" ! "href"
+  return $ nub links
--- main.hs
+++ main.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
 import Web.Blog.Type
 import Network.URI
 import Text.XML.HXT.Core
 import Text.HandsomeSoup
 import Network.HTTP.Conduit
-import Data.ByteString.Lazy.Char8 as BL8
+import Data.ByteString.Lazy.Char8 as BL8 (pack, unpack)
 import Control.Applicative
 
 main = do
@@ -13,6 +16,9 @@ main = do
   print b
   print a2
 
+hss = do
+--   c <- simpleHttp "http://d.hatena.ne.jp/suzuki-shin/archive"
   c <- simpleHttp "http://d.hatena.ne.jp/kazu-yamamoto/archive"
-  let doc = readString [withParseHTML yes, withWarnings no] $ BL8.pack c
-  print doc
+  let doc = readString [withParseHTML yes, withWarnings no] $ BL8.unpack c
+  links <- runX $ doc //> css "div" >>> hasAttrValue "id" (== "pager-top") >>> css "a" ! "href"
+  mapM_ putStrLn links
